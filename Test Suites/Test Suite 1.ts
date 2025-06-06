681cb701cf85fa3dc3ddae853ba9360f14ae2343
SonHuynh-katalon <son.huynh@katalon.com>
2025-06-06 11:08:15 +0700

    Create test suite

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>83bb244b-db16-47f2-9765-10892d1afa6a</testSuiteGuid>
+</TestSuiteEntity>
